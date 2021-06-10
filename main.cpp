a47611255239de5a7250a867f10dbf385659e724
Uriel <83469583+JustAsteri@users.noreply.github.com>
2021-06-10 15:32:00 +0000

    Version 1.2.1 Change in winners' restriction

--- main.cpp
+++ main.cpp
@@ -8,28 +8,31 @@ int main()
 {
   int numero, boletos_vendidos, cant_ganadores;
   int limiteBoletos = 1000;
+
   srand(time(0));
 
   cout << "cuantos boletos fueron vendidos? (Valores validos: 1 - 1000)"<< endl;
   cin>>boletos_vendidos;
-  cout << "cuantos primeros lugares? (Valores validos: 1 - 1000)"<< endl;
+  cout << "cuantos ganadores hay? (Valores validos: 1 - 1000)"<< endl;
   cin>>cant_ganadores;
 
-  if (
-    (boletos_vendidos <= limiteBoletos && boletos_vendidos > 0) 
+
+  if //Restrictions 
+  (
+    (boletos_vendidos <= limiteBoletos && boletos_vendidos > 1) 
     && 
-    (cant_ganadores <= limiteBoletos && cant_ganadores > 0)
-    )
+    (cant_ganadores < boletos_vendidos && cant_ganadores > 0)
+  )
   {
-  for(int x = 1; x <= cant_ganadores; x++)
+    for(int x = 1; x <= cant_ganadores; x++)  
     {
-      numero = rand() % boletos_vendidos + 1;
-      cout << "el " << x << " lugar es el boleto numero: " << numero << endl;
+      numero = rand() % boletos_vendidos + 1; //Creating random numbers
+      cout << "El " << x << " lugar es el boleto numero: " << numero << endl;
     }
   }
   else 
   {
-    cout << "Ingresa valores validos entre 1 y " << limiteBoletos << endl;
+    cout << "Ingresa valores validos"<< endl;
   }  
 
   return 0;
