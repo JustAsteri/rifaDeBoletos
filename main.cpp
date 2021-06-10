4d63be59eb21bfe71c52a40e3c46c8acba6176e8
Uriel <83469583+JustAsteri@users.noreply.github.com>
2021-06-10 14:52:02 +0000

    Version 1.2 Add limits in tickets and winners

--- main.cpp
+++ main.cpp
@@ -6,19 +6,31 @@ using namespace std;
 
 int main()
 {
-    int numero, boletos_vendidos, cant_ganadores;
-    srand(time(0));
+  int numero, boletos_vendidos, cant_ganadores;
+  int limiteBoletos = 1000;
+  srand(time(0));
 
-    cout << "cuantos boletos fueron vendidos?"<< endl;
-    cin>>boletos_vendidos;
-    cout << "cuantos primeros lugares?"<< endl;
-    cin>>cant_ganadores;
+  cout << "cuantos boletos fueron vendidos? (Valores validos: 1 - 1000)"<< endl;
+  cin>>boletos_vendidos;
+  cout << "cuantos primeros lugares? (Valores validos: 1 - 1000)"<< endl;
+  cin>>cant_ganadores;
 
-    for(int x=1; x<=cant_ganadores; x++)
+  if (
+    (boletos_vendidos <= limiteBoletos && boletos_vendidos > 0) 
+    && 
+    (cant_ganadores <= limiteBoletos && cant_ganadores > 0)
+    )
+  {
+  for(int x = 1; x <= cant_ganadores; x++)
     {
-        numero=rand()%boletos_vendidos+1;
-        cout << "el "<<x<<" lugar es el boleto numero: "<<numero<< endl;
+      numero = rand() % boletos_vendidos + 1;
+      cout << "el " << x << " lugar es el boleto numero: " << numero << endl;
     }
+  }
+  else 
+  {
+    cout << "Ingresa valores validos entre 1 y " << limiteBoletos << endl;
+  }  
 
-    return 0;
+  return 0;
 }
