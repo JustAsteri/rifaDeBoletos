cfb756eceed08a54fc99ea2f30fd6f8eec956266
Uriel <83469583+JustAsteri@users.noreply.github.com>
2021-06-10 14:33:28 +0000

    Version 1

--- main.cpp
+++ main.cpp
@@ -1,6 +1,16 @@
 #include <iostream>
-#include 
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
 
 int main() {
+  srand(time(0));
+  int rangoAzar = 50;
   
+  for (int i = 1; i <= 3; i++)
+  {
+    int numero = rand() % rangoAzar + 1;
+    cout << "El " << i << " lugar es el boleto numero: " << numero << endl; 
+  }
 }
