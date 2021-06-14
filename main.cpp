7904042ad3852aa0afcfd7146fd60c754c974f9e
Uriel <83469583+JustAsteri@users.noreply.github.com>
2021-06-14 15:23:31 +0000

    Version 1.4 Added function and loop to avoid repeat numbers

--- main.cpp
+++ main.cpp
@@ -4,9 +4,19 @@
 
 using namespace std;
 
+//Function to know if a number is repeat it
+bool checkrep(int n, int num[], int boletos_vendidos)
+{
+    for(int i=0; i<boletos_vendidos; i++)
+        if(n == num[i])
+            return true;
+    return false;
+}
+//***************************
+
 int main()
 {
-  int numero, boletos_vendidos, cant_ganadores, repetirRifa = 1;
+  int n, numero[1000], boletos_vendidos, cant_ganadores, repetirRifa = 1;
   int limiteBoletos = 1000;
 
   srand(time(0));
@@ -22,14 +32,21 @@ int main()
     cin>>cant_ganadores;
     cout << endl;
 
-    //Restrictions--
+    //Restrictions
     if
     ((boletos_vendidos <= limiteBoletos && boletos_vendidos > 1) && (cant_ganadores < boletos_vendidos && cant_ganadores > 0))
     {
       for(int x = 1; x <= cant_ganadores; x++)  
       {
-        numero = rand() % boletos_vendidos + 1; //Creating random numbers
-        cout << "El " << x << " lugar es el boleto numero: " << numero << endl;
+        //Avoid repeat random numbers
+        do
+        {
+          n = 1 + rand() % boletos_vendidos;
+        }
+        while (checkrep(n, numero, boletos_vendidos));
+        numero[x] = n;
+        //***************************
+        cout << "El " << x << " lugar es el boleto numero: " << n << endl;
       }
     }
     else 
@@ -37,7 +54,7 @@ int main()
       cout << endl;
       cout << "Ingresa valores validos"<< endl;
     }
-    //--Restrictions 
+    //*************************** 
 
     //Loop for repeat the raffle --
     cout << endl;
@@ -51,7 +68,7 @@ int main()
         cout << "--------------> ";
         cin >> repetirRifa;
     }
-    //--Loop for repeat the raffle
+    //***************************
 
     //Exit the game
     if (repetirRifa == 0)
@@ -59,7 +76,9 @@ int main()
       cout << endl;
      cout << "Gracias por usar el programa numero uno de rifas" << endl;
     }
+    //***************************
   }
+  //***************************
   
   return 0;
 }
