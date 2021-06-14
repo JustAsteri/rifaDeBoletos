1ecac4e5839f86aa61daa27e04d8c9b067b08869
Uriel <83469583+JustAsteri@users.noreply.github.com>
2021-06-14 17:16:14 +0000

    Version 1.5 Fixed restrictions to the user data input and improve interface

--- main.cpp
+++ main.cpp
@@ -4,13 +4,22 @@
 
 using namespace std;
 
+/*
+  Error cuando ingresan dato no numerico
+
+*/
+
 //Function to know if a number is repeat it
 bool checkrep(int n, int num[], int boletos_vendidos)
 {
-    for(int i=0; i<boletos_vendidos; i++)
-        if(n == num[i])
-            return true;
-    return false;
+  for(int i=0; i<boletos_vendidos; i++)
+  {
+    if(n == num[i])
+    {
+      return true;
+    }
+  } 
+  return false;
 }
 //***************************
 
@@ -24,61 +33,84 @@ int main()
   //Repeat the raffle
   while (repetirRifa == 1) 
   {
-    cout << "cuantos boletos fueron vendidos? (Valores validos: 1 - 1000)"<< endl;
+    cout << endl;
+    cout << "****************************************************************" << endl;
+    cout << "| cuantos boletos fueron vendidos? (Valores validos: 1 - 1000) |" << endl;
+    cout << "****************************************************************"<< endl;
     cout << "--------------> ";
     cin>>boletos_vendidos;
-    cout << "cuantos ganadores hay? (Valores validos: 1 - 1000)"<< endl;
+      //Restrictions
+      while (boletos_vendidos < 1 || boletos_vendidos > 1000)
+      {
+        cout << endl;
+        cout << "Ingrese numeros validos" << endl;
+        cout << endl;
+        cout << "****************************************************************" << endl;
+        cout << "| cuantos boletos fueron vendidos? (Valores validos: 1 - 1000) |" << endl;
+        cout << "****************************************************************"<< endl;
+        cout << "--------------> ";
+        cin>>boletos_vendidos;
+      }
+    cout << endl;
+    cout << "******************************************************"<< endl;
+    cout << "| cuantos ganadores hay? (Valores validos: 1 - 1000) |" << endl;
+    cout << "******************************************************"<< endl;
     cout << "--------------> ";
     cin>>cant_ganadores;
+      //Restrictions
+      while (cant_ganadores < 1 || cant_ganadores > boletos_vendidos)
+      {
+        cout << endl;
+        cout << "Ingrese numeros validos" << endl;
+        cout << endl;
+        cout << "******************************************************"<< endl;
+        cout << "| cuantos ganadores hay? (Valores validos: 1 - 1000) |" << endl;
+        cout << "******************************************************"<< endl;
+        cout << "--------------> ";
+        cin>>boletos_vendidos;
+      }
+    cout << endl;
     cout << endl;
 
-    //Restrictions
-    if
-    ((boletos_vendidos <= limiteBoletos && boletos_vendidos > 1) && (cant_ganadores < boletos_vendidos && cant_ganadores > 0))
+    for(int x = 1; x <= cant_ganadores; x++)  
     {
-      for(int x = 1; x <= cant_ganadores; x++)  
+      //Avoid repeat random numbers
+      do
       {
-        //Avoid repeat random numbers
-        do
-        {
-          n = 1 + rand() % boletos_vendidos;
-        }
-        while (checkrep(n, numero, boletos_vendidos));
-        numero[x] = n;
-        //***************************
-        cout << "El " << x << " lugar es el boleto numero: " << n << endl;
+        n = 1 + rand() % boletos_vendidos;
       }
+      while (checkrep(n, numero, boletos_vendidos));
+      numero[x] = n;
+      //***************************
+      cout << "*************************************************"<< endl;
+      cout << "| El ganador numero " << x << " es el boleto numero: " << n << endl;
+      cout << "*************************************************"<< endl;
     }
-    else 
-    {
-      cout << endl;
-      cout << "Ingresa valores validos"<< endl;
-    }
-    //*************************** 
+  }
 
-    //Loop for repeat the raffle --
+  //Loop for repeat the raffle
+  cout << endl;
+  cout << "Desea realizar otra rifa? \n 1 = Si\n 0 = No" << endl;
+  cout << "--------------> ";
+  cin >> repetirRifa;
+  
+  while (repetirRifa != 1 && repetirRifa != 0)
+  {
+    cout << endl;
+    cout << "Ingresa un valor valido (1 o 0): " << endl;
     cout << endl;
     cout << "Desea realizar otra rifa? \n 1 = Si\n 0 = No" << endl;
     cout << "--------------> ";
     cin >> repetirRifa;
-    
-    while (repetirRifa != 1 && repetirRifa != 0)
-    {
-        cout << "Ingresa un valor valido (1 o 0): " << endl;
-        cout << "--------------> ";
-        cin >> repetirRifa;
-    }
-    //***************************
-
-    //Exit the game
-    if (repetirRifa == 0)
-    {
-      cout << endl;
-     cout << "Gracias por usar el programa numero uno de rifas" << endl;
-    }
-    //***************************
   }
   //***************************
-  
+
+  //Exit the game
+  if (repetirRifa == 0)
+  {
+    cout << endl;
+    cout << "Gracias por usar el programa numero uno de rifas" << endl;
+  }
+  //***************************
   return 0;
 }
