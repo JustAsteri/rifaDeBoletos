62e82c6b8c33537d1c5ecaea2fcc2cbccf9cf441
Uriel <83469583+JustAsteri@users.noreply.github.com>
2021-06-15 15:03:14 +0000

    Version 1.5.2 Fixed interface and loops if the user gives out of range values

--- main.cpp
+++ main.cpp
@@ -1,13 +1,11 @@
 #include <iostream>
 #include <cstdlib>
 #include <ctime>
+#include <cmath>
+#include <stdlib.h>
+//#include <windows.h>
 
 using namespace std;
-
-/*
-  Error cuando ingresan dato no numerico
-
-*/
 
 //Function to know if a number is repeat it
 bool checkrep(int n, int num[], int boletos_vendidos)
@@ -26,6 +24,7 @@ bool checkrep(int n, int num[], int boletos_vendidos)
 int main()
 {
   int n, numero[1000], boletos_vendidos, cant_ganadores;
+  string letras_boletos_vendidos, letras_cant_ganadores;
   bool validarEntradas;
   string repetirRifa;
   int limiteBoletos = 1000;
@@ -33,44 +32,92 @@ int main()
   srand(time(0));
 
   //Repeat the raffle
-  do
+  do //Loop if the user wants to do another raffle
   {
     cout << endl;
     cout << "****************************************************************" << endl;
-    cout << "| cuantos boletos fueron vendidos? (Valores validos: 1 - 1000) |" << endl;
-    cout << "****************************************************************"<< endl;
-    cout << "--------------> ";
-    cin>>boletos_vendidos;
-      //Restrictions
-      while (boletos_vendidos < 1 || boletos_vendidos > 1000)
+    cout << "| Bienvenido al mejor programa de generacion de Rifas que hay! |" << endl;
+    cout << "****************************************************************" << endl;
+    do //Loop if the user gives out of range value
+    {
+      cout << endl;
+      cout << "******************************************************************************" << endl;
+      cout << "| cuantos boletos fueron vendidos? (Valores validos: Numeros entre 1 y 1000) |" << endl;
+      cout << "******************************************************************************" << endl;
+      cout << "--------------> ";
+      do //Loop to check if the value is valid (numbers) 
       {
-        cout << endl;
-        cout << "Ingrese numeros validos" << endl;
-        cout << endl;
-        cout << "****************************************************************" << endl;
-        cout << "| cuantos boletos fueron vendidos? (Valores validos: 1 - 1000) |" << endl;
-        cout << "****************************************************************"<< endl;
-        cout << "--------------> ";
-        cin>>boletos_vendidos;
-      }
-    cout << endl;
-    cout << "******************************************************"<< endl;
-    cout << "| cuantos ganadores hay? (Valores validos: 1 - 1000) |" << endl;
-    cout << "******************************************************"<< endl;
-    cout << "--------------> ";
-    cin>>cant_ganadores;
-      //Restrictions
-      while (cant_ganadores < 1 || cant_ganadores > boletos_vendidos)
+        try 
+        {
+          cin>>letras_boletos_vendidos;
+          boletos_vendidos = stoi(letras_boletos_vendidos);
+          if (boletos_vendidos == false)
+          {
+            cout << endl;
+            cout << "Ingrese numeros validos" << endl;
+            validarEntradas = false;
+          }
+          else
+          {
+            if (boletos_vendidos < 1 || boletos_vendidos > 1000)
+            {
+              cout << endl;
+              cout << "Ingrese numeros validos" << endl;
+              cout << endl;
+            }
+            validarEntradas = true;
+          }
+        }
+        catch (exception e)
+        {
+          cout << endl;
+          cout << "Ingrese numeros validos" << endl;
+          cout << endl;
+          validarEntradas = true;
+        } 
+      } while (validarEntradas == false);
+    } while (boletos_vendidos < 1 || boletos_vendidos > 1000);
+
+    do
+    {
+      cout << endl;
+      cout << "*******************************************************************"<< endl;
+      cout << "| cuantos ganadores hay? (Valores validos: Numeros entre 1 y " << boletos_vendidos << "" ") |" << endl;
+      cout << "*******************************************************************"<< endl;
+      cout << "--------------> ";
+      do //Loop to check if the value is valid (numbers) 
       {
-        cout << endl;
-        cout << "Ingrese numeros validos" << endl;
-        cout << endl;
-        cout << "******************************************************"<< endl;
-        cout << "| cuantos ganadores hay? (Valores validos: 1 - 1000) |" << endl;
-        cout << "******************************************************"<< endl;
-        cout << "--------------> ";
-        cin>>boletos_vendidos;
-      }
+        try 
+        {
+          cin>>letras_cant_ganadores;
+          cant_ganadores = stoi(letras_cant_ganadores);
+          if (cant_ganadores == false)
+          {
+            cout << endl;
+            cout << "Ingrese numeros validos" << endl;
+            validarEntradas = false;
+          }
+          else
+          {
+            if ((cant_ganadores < 1 || cant_ganadores > boletos_vendidos))
+            {
+              cout << endl;
+              cout << "Ingrese numeros validos" << endl;
+              cout << endl;
+            }
+            validarEntradas = true;
+          }
+          cout << endl;
+        }
+        catch (exception e)
+        {
+          cout << endl;
+          cout << "Ingrese numeros validos" << endl;
+          cout << endl;
+          validarEntradas = true;
+        } 
+      } while (validarEntradas == false);
+    } while (cant_ganadores < 1 || cant_ganadores > boletos_vendidos);
     cout << endl;
     cout << endl;
 
@@ -92,6 +139,8 @@ int main()
     cout << "Desea realizar otra rifa? \n 1 = Si\n Cualquier otra tecla = No" << endl;
     cout << "--------------> ";
     cin >> repetirRifa;
+    //system("clear"); //Linux
+    system("cls"); //Windows
   }
   while (repetirRifa == "1");
 
